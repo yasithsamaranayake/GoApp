7c0a3bf2500f955d6cec886d38c91b94b2717ba9
yasithsamaranayake <ysamaran@our.ecu.edu.au>
2025-03-28 15:39:22 +0530

    Updated main.go

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ func main() {
 	var name string
 	var age int
 
-	fmt.Print("Hi, what is your name? ")
+	fmt.Print("Welcome to Yasith's first Go program. Hi, what is your name? ")
 	fmt.Scanln(&name)
 
 	fmt.Print("How old are you? ")
 	fmt.Scanln(&age)
 
-	fmt.Println("Hi, your name is", name, "and you are", age, "years old.")
+	fmt.Println("Hi, your name is", name, "and you are", age, "years old.\nThank you for using this program. Goodbye...")
 }
